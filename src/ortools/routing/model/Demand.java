efef3bc8edf3a1c3aef2946ebb2f5cb3f4c773a3
Anis <muhammed-anis.messaoudi@uha.fr>
2019-06-02 16:14:26 +0200

    read demand file

--- src/ortools/routing/model/Demand.java
+++ src/ortools/routing/model/Demand.java
@@ -1,5 +1,33 @@
 package ortools.routing.model;
 
 public class Demand {
+    
+    private String id;
+    private int isUrgent;
+    private String request;
+    private String originId;
+    private String destinationId;
+    private long deliveryTime;
+    private String state;
+    
+    public Demand(String id, int isUrgent, String request, String originId, String destinationId, long deliveryTime,
+	    String state) {
+	super();
+	this.id = id;
+	this.isUrgent = isUrgent;
+	this.request = request;
+	this.originId = originId;
+	this.destinationId = destinationId;
+	this.deliveryTime = deliveryTime;
+	this.state = state;
+    }
+
+    @Override
+    public String toString() {
+	return "Demand [id=" + id + ", isUrgent=" + isUrgent + ", request=" + request + ", originId=" + originId
+		+ ", destinationId=" + destinationId + ", deliveryTime=" + deliveryTime + ", state=" + state + "]";
+    }
+    
+    
 
 }
--- src/ortools/routing/util/JsonDecryptor.java
+++ src/ortools/routing/util/JsonDecryptor.java
@@ -75,16 +75,9 @@ public class JsonDecryptor {
 	    JSONArray jsonArray = (JSONArray) jsonObject.get("mailboxes"); 
 	    for(int i=0;i<jsonArray.size();i++) {
 		JSONObject job2=(JSONObject) jsonArray.get(i);
-		
 		mailboxes.add(new Mailbox((String)job2.get("name"), (Boolean)job2.get("isEmpty")));
-		System.out.println( job2.get("name")+ " : " + job2.get("isEmpty"));
-
 	    }
-	    
 	    vehicles.add(new Vehicle(id, name, energy, destinationId, mailboxes));
-
-	    	   
-	    
 	}
 	catch(Exception e) {
 	    e.printStackTrace();
@@ -92,11 +85,60 @@ public class JsonDecryptor {
     }
     
     
+    @SuppressWarnings("rawtypes")
     public void readDemandFile() {
 	JSONParser parser = new JSONParser();
 	try {
 	    Object obj = parser.parse(new FileReader("jsonFiles/demands.json"));
 	    JSONObject jsonObject = (JSONObject) obj;
+	    
+	    String id= "";
+	    int isUrgent=0;
+	    String request="";
+	    String originId="";
+	    String destinationId="";
+	    long deliveryTime=0;
+	    String state="";
+	    JSONArray jsonArray = (JSONArray) parser.parse(jsonObject.get("Demands").toString());
+	    
+	    for(int i=0;i<jsonArray.size();i++) {
+		JSONObject job2=(JSONObject) jsonArray.get(i);
+		for(Iterator iterator2 = job2.keySet().iterator(); iterator2.hasNext();) {
+		    String key = (String) iterator2.next();
+		    if ( key.equals("_id")) {
+			JSONObject jsonObject3 = (JSONObject) parser.parse(job2.get(key).toString());
+			id=jsonObject3.get("$oid").toString();
+		    }
+		    if ( key.equals("isUrgent")) {
+			JSONObject jsonObject3 = (JSONObject) parser.parse(job2.get(key).toString());
+			isUrgent=Integer.parseInt(jsonObject3.get("$numberInt").toString());
+		    }
+		    if ( key.equals("request")) {
+			request= job2.get(key).toString();
+		    }
+		    if ( key.equals("origin")) {
+			JSONObject jsonObject3 = (JSONObject) parser.parse(job2.get(key).toString());
+			JSONObject job3 =(JSONObject) jsonObject3.get("_id");
+			originId = job3.get("$oid").toString();
+		    }
+		    if ( key.equals("destination")) {
+			JSONObject jsonObject3 = (JSONObject) parser.parse(job2.get(key).toString());
+			JSONObject job3 =(JSONObject) jsonObject3.get("_id");
+			destinationId = job3.get("$oid").toString();
+		    }
+		    if ( key.equals("deliveryTime")) {
+			JSONObject jsonObject3 = (JSONObject) parser.parse(job2.get(key).toString());
+			JSONObject job3 =(JSONObject) jsonObject3.get("$date");
+			deliveryTime = Long.parseLong(job3.get("$numberLong").toString());
+		    }
+		    if ( key.equals("state")) {
+			state= job2.get(key).toString();
+		    }
+		}
+		Demand d = new Demand(id, isUrgent, request, originId, destinationId, deliveryTime, state);
+		System.out.println(d.toString());
+		demands.add(d);	    
+		}
 	}
 	catch(Exception e) {
 	    e.printStackTrace();
