70a1a5069231c234242a2b91b6c440a7ea74c771
Anis <muhammed-anis.messaoudi@uha.fr>
2019-06-13 17:15:06 +0200

    coorect id pb in java encryptor

--- src/fr/uha/ensisa/project/smartUHA/util/JsonEncryptor.java
+++ src/fr/uha/ensisa/project/smartUHA/util/JsonEncryptor.java
@@ -43,22 +43,26 @@ public class JsonEncryptor {
 	    for (int j = 1; j < dt.getNodeNumber() - 1; j++) {
 		JSONObject destination = new JSONObject();
 		JSONObject destinationId = new JSONObject();
-		destinationId.put("$oid", dt.getNodeTable().get((int)solution[i][j]));
+		int location = (int) solution[i][j];
+		destinationId.put("$oid", dt.getNodeTable().get(location));
 		destination.put("destinationId", destinationId);
 		
 		JSONArray demands = new JSONArray();
 		JSONObject demand = new JSONObject();
-		demand.put("$oid", dt.getDemands().get((j+1)/2-1).getId());
+		demand.put("$oid", dt.getDemands().get((location+1)/2 -1 ).getId());
 		demands.add(demand);
 		
-		String actualDestId = dt.getNodeTable().get((int)solution[i][j]);
+		String actualDestId = dt.getNodeTable().get(location);
 		if(j < dt.getNodeNumber() - 2 ) {
-        		while(actualDestId.equals(dt.getNodeTable().get((int)solution[i][j+1]))) {
-        		    JSONObject demand2 = new JSONObject();
-        		    demand2.put("$oid", dt.getNodeTable().get((int)solution[i][j+1]));
-        		    demands.add(demand2);
-        		    j++;
-        		}
+		    while(actualDestId.equals(dt.getNodeTable().get((int) solution[i][j+1]))) {
+			System.out.println(dt.getNodeTable().get(location));
+			JSONObject demand2 = new JSONObject();
+			demand2.put("$oid", dt.getDemands().get((j-dt.getVehicleNumber()+1)/2).getId());
+			demands.add(demand2);
+			j++;
+			if (j >=dt.getNodeNumber() - 2 )
+			    break;
+		    }
 		}
 		destination.put("demandId", demands);
 		
